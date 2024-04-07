edc6da6a0a0516b532ac0e92f6ae61aec54ce42c
ilya-vor <157091996+ilya-vor@users.noreply.github.com>
2024-04-07 11:26:55 +0500

    Переместил методы модели в геттеры и сеттеры

--- Form1.cs
+++ Form1.cs
@@ -7,58 +7,88 @@ namespace LW4_OOP_Part2
         {
             InitializeComponent();
             model = new Model();
-            model.observers += new EventHandler(update_from_model);
+            model.DataChanged += new EventHandler(update_from_model);
             this.update_from_model(this, null);
         }
         private void textBox_TextChanged(object sender, EventArgs e)
         {
-            int previous_number = model.getB();
             if (sender == textBox_A)
-                model.setA(int.Parse(textBox_A.Text));
+                model.A = int.Parse(textBox_A.Text);
             else if (sender == textBox_B)
-                model.setB(int.Parse(textBox_B.Text), previous_number);
+                model.B = int.Parse(textBox_B.Text);
             else
-                model.setC(int.Parse(textBox_C.Text));
+                model.C = int.Parse(textBox_C.Text);
         }
         private void numericUpDown_ValueChanged(object sender, EventArgs e)
         {
-            int previous_number = model.getB();
             if (sender == numericUpDown_A)
-                model.setA(Decimal.ToInt32(numericUpDown_A.Value));
+                model.A = Decimal.ToInt32(numericUpDown_A.Value);
             else if (sender == numericUpDown_B)
-                model.setB(Decimal.ToInt32(numericUpDown_B.Value), previous_number);
+                model.B = Decimal.ToInt32(numericUpDown_B.Value);
             else
-                model.setC(Decimal.ToInt32(numericUpDown_C.Value));
+                model.C = Decimal.ToInt32(numericUpDown_C.Value);
         }
         private void trackBar_ValueChanged(object sender, EventArgs e)
         {
-            int previous_number = model.getB();
             if (sender == trackBar_A)
-                model.setA(Decimal.ToInt32(trackBar_A.Value));
+                model.A = Decimal.ToInt32(trackBar_A.Value);
             else if (sender == trackBar_B)
-                model.setB(Decimal.ToInt32(trackBar_B.Value), previous_number);
+                model.B = Decimal.ToInt32(trackBar_B.Value);
             else
-                model.setC(Decimal.ToInt32(trackBar_C.Value));
+                model.C = Decimal.ToInt32(trackBar_C.Value);
         }
         public void update_from_model(object sender, EventArgs e)
         {
-            textBox_A.Text = model.getA().ToString();
-            textBox_B.Text = model.getB().ToString();
-            textBox_C.Text = model.getC().ToString();
-            numericUpDown_A.Value = model.getA();
-            numericUpDown_B.Value = model.getB();
-            numericUpDown_C.Value = model.getC();
-            trackBar_A.Value = model.getA();
-            trackBar_B.Value = model.getB();
-            trackBar_C.Value = model.getC();
+            textBox_A.Text = model.A.ToString();
+            textBox_B.Text = model.B.ToString();
+            textBox_C.Text = model.C.ToString();
+            numericUpDown_A.Value = model.A;
+            numericUpDown_B.Value = model.B;
+            numericUpDown_C.Value = model.C;
+            trackBar_A.Value = model.A;
+            trackBar_B.Value = model.B;
+            trackBar_C.Value = model.C;
         }
     }
     class Model
     {
-        private int A;
-        private int B;
-        private int C;
-        public System.EventHandler observers;
+        public System.EventHandler DataChanged;
+        private int _A;
+        private int _B;
+        private int _C;
+        public int A
+        {
+            get{ return _A; }
+            set
+            {
+                selectedValueInRange(ref value);
+                if (value > B) _A = B;
+                else _A = value;
+                DataChanged?.Invoke(this, null);
+            }
+        }
+        public int B
+        {
+            get
+            { return _B; }
+            set
+            {
+                selectedValueInRange(ref value);
+                if (value <= C && value >= A) _B = value;
+                DataChanged?.Invoke(this, null);
+            }
+        }
+        public int C
+        {
+            get { return _C; }
+            set
+            {
+                selectedValueInRange(ref value);
+                if (value < B) _C = B;
+                else _C = value;
+                DataChanged?.Invoke(this, null);
+            }
+        }
         public int selectedValueInRange(ref int value)
         {
             if (value < 0) value = 0;
@@ -67,31 +97,7 @@ namespace LW4_OOP_Part2
         }
         public Model()
         {
-            A = 10; B = 50; C = 100;
+            C = 100; B = 50; A = 10;
         }
-        public void setA(int value)
-        {
-            selectedValueInRange(ref value);
-            if (value > B) A = B;
-            else A = value;
-            observers.Invoke(this, null);
-        }
-        public void setC(int value)
-        {
-            selectedValueInRange(ref value);
-            if (value < B) C = B;
-            else C = value;
-            observers.Invoke(this, null);
-        }
-        public void setB(int value, int previous_number)
-        {
-            selectedValueInRange(ref value);
-            if (value > C || value < A) B = previous_number;
-            else B = value;
-            observers.Invoke(this, null);
-        }
-        public int getA() { return A; }
-        public int getB() { return B; }
-        public int getC() { return C; }
     }
 }
